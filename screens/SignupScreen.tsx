dd8953f5487ccc3e176bac09f44a80128e0d6583
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-12-01 11:48:41 +0200

    update signup screen

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,8 +1,11 @@
+import React, { useContext } from "react";
 import { Alert } from "react-native";
 import AuthenticationContent from "../components/auth/AuthenticationContent";
 import { createNewUser } from "../util/authentication";
+import { AuthenticationContext } from "../store/AuthenticationContext";
 
 const SignupScreen = () => {
+  const authenticationContext = useContext(AuthenticationContext);
   const signingUpHandler = async ({
     email,
     password,
@@ -13,6 +16,7 @@ const SignupScreen = () => {
     try {
       const response = await createNewUser(email, password);
       Alert.alert("response", JSON.stringify(response));
+      authenticationContext.authenticate(response.idToken);
     } catch (error) {
       Alert.alert("An error occurred!", JSON.stringify(error));
     }
