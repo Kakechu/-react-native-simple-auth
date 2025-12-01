0ebd0975d55492330b4859faeaacfb2b66554450
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-12-01 10:44:27 +0200

    add SignupScreen (some stuff missing)

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,7 +1,23 @@
-import AuthenticationContent from '../components/auth/AuthenticationContent';
+import { Alert } from "react-native";
+import AuthenticationContent from "../components/auth/AuthenticationContent";
+import { createNewUser } from "../util/authentication";
 
 const SignupScreen = () => {
-    return <AuthenticationContent />;
-}
+  const signingUpHandler = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const response = await createNewUser(email, password);
+      Alert.alert("response", JSON.stringify(response));
+    } catch (error) {
+      Alert.alert("An error occurred!", JSON.stringify(error));
+    }
+  };
+  return <AuthenticationContent onAuthentication={signingUpHandler} />;
+};
 
 export default SignupScreen;
